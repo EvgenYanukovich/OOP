40873dd543507c0905018cc292b22a7fb3976716
Evgeniy Yanukovich <86297837+EvgenYanukovich@users.noreply.github.com>
2023-10-19 12:11:24 +0300

    Add files via upload

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace lab2 { 
+    public partial class Set
+    {
+        public const string name = "Set";
+        private static int instanceCount;
+        private readonly int id;
+        private List<int> elements;
+
+        static Set()
+        {
+            instanceCount = 0;
+        }
+
+        private Set(List<int> elements, int? id)
+        {
+            this.elements = elements;
+            this.id = id ?? this.GetHashCode();
+            instanceCount++;
+        }
+        
+        public Set() : this(new List<int>())
+        {
+            
+        }
+
+        public Set(IEnumerable<int> elements): this(new List<int>(elements), null)
+        {
+
+        }
+
+        public Set(int element = 5) : this(new List<int> {element})
+        {
+            
+        }
+        
+        public Set(Set set): this(new List<int>(set.elements), set.id)
+        {
+
+        }
+
+        public static int InstanceCount
+        {
+            get { return instanceCount; }
+        }
+
+        public int ID
+        {
+            get { return id; }
+        }
+
+        public List<int> Elements
+        {
+            get { return elements; }
+            set { elements = value; }
+        }
+
+        public void AddElement(int element)
+        {
+            if (!elements.Contains(element))
+                elements.Add(element);
+        }
+
+        public void RemoveElement(int element)
+        {
+            elements.Remove(element);
+        }
+
+        public void BoolAdd(ref int element, out bool val)
+        {
+            if (!elements.Contains(element))
+            {
+                val = true;
+                elements.Add(element);
+            }
+            else
+            {
+                val = false;
+            }
+        }
+        public static string Information()
+        {
+            return $"Имя: {name}\nКол-во экземпляров: {instanceCount}" ;
+        }
+
+        public static Set Intersection(Set set1, Set set2)
+        {
+            return new Set(set1.elements.Intersect(set2.elements).ToList());
+        }
+
+        public static Set Difference(Set set1, Set set2)
+        {
+            return new Set(set1.elements.Except(set2.elements).ToList());
+        }
+
+        public override string ToString()
+        {
+            return "Set ID: " + id + ", Elements: " + string.Join(", ", elements);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(id, elements, ID, Elements);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Set set &&
+                   id == set.id &&
+                   EqualityComparer<List<int>>.Default.Equals(elements, set.elements) &&
+                   ID == set.ID &&
+                   EqualityComparer<List<int>>.Default.Equals(Elements, set.Elements);
+        }
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Set set1 = new Set(new List<int> { 1, 2, 3, 4 });
+            Set set2 = new Set(new List<int> { 3, 4, 5, 6 });
+            Set set3 = new Set(new List<int> { -1, -2, -3, -4 });
+
+            Console.WriteLine("Set1: " + set1.ToString());
+            Console.WriteLine("Set2: " + set2.ToString());
+            Console.WriteLine("Set3: " + set3.ToString());
+
+            Set[] sets = new Set[] { set1, set2, set3 };
+
+            foreach (var set in sets)
+            {
+                if (set.Elements.All(x => x % 2 == 0))
+                    Console.WriteLine("Only even elements: " + set.ToString());
+
+                if (set.Elements.All(x => x % 2 != 0))
+                    Console.WriteLine("Only odd elements: " + set.ToString());
+
+                if (set.Elements.Any(x => x < 0))
+                    Console.WriteLine("Contains negative elements: " + set.ToString());
+            }
+
+            var anonymousType = new { ID = 1, Elements = new List<int> { 1, 2, 3 } };
+            Console.WriteLine(anonymousType);
+        }
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+public class Password
+{
+    private string password;
+
+    //Конструктор
+    public Password(string password)
+    {
+        this.password = password;
+    }
+
+    //Индексатор
+    public string this[string password] 
+    {
+        get {return password;}
+        set {password = value;}
+    }
+    
+    public static Password operator -(Password p, int item)
+    {
+        p.password = p.password.Remove(p.password.Length - 1);
+        return p;
+    }
+
+    public static bool operator >(Password p1, Password p2)
+    {
+        return p1.password.Length > p2.password.Length;
+    }
+
+    public static bool operator <(Password p1, Password p2)
+    {
+        return p1.password.Length < p2.password.Length;
+    }
+
+    public static bool operator !=(Password p1, Password p2)
+    {
+        return p1.password != p2.password;
+    }
+
+    public static bool operator ==(Password p1, Password p2)
+    {
+        return p1.password == p2.password;
+    }
+
+    public static Password operator ++(Password p)
+    {
+        p.password = "default";
+        return p;
+    }
+
+    public static bool operator true(Password p)
+    {
+        return p.password.Length >= 8;
+    }
+
+    public static bool operator false(Password p)
+    {
+        return p.password.Length < 8;
+    }
+
+    public class Production
+    {
+        public int Id { get; set; }
+        public string OrganizationName { get; set; }
+    }
+    dynamic Product = new Production() { Id = 0 , OrganizationName = "Moloko"};
+
+    public class Developer
+    {
+        public string FullName { get; set; }
+        public int Id { get; set; }
+        public string Department { get; set; }
+    }
+    dynamic Dev = new Developer() { FullName = "Evgeniy", Id = 0, Department="BSTU" };
+}
+
+public static class StatisticOperation
+{
+    public static int Sum(Password p1, Password p2)
+    {
+        return p1.ToString().Length + p2.ToString().Length;
+    }
+
+    public static int Minus(Password p1, Password p2)
+    {
+        return p1.ToString().Length >= p2.ToString().Length ? p1.ToString().Length - p2.ToString().Length : p2.ToString().Length - p1.ToString().Length;
+    }
+
+    public static int Length(Password p1)
+    {
+        return p1.ToString().Length;
+    }
+
+    public static string MiddleSymbol(this string str)
+    {
+        return str.Length % 2 == 0 ? str.Substring(str.Length / 2 - 1, 2) : str.Substring(str.Length / 2, 1);
+    }
+
+    public static bool IsValidLength(this Password password)
+    {
+        var length = password.ToString().Length;
+        return length >= 6 && length <= 12;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Password password1 = new Password("password1");
+        Password password2 = new Password("password2");
+
+        //-
+        password1 = password1 - 1;
+        Console.WriteLine($"Password after - operator?\n {password1}");
+
+        //>
+        bool isPassword1Longer = password1 > password2;
+        Console.WriteLine($"Is password1 longer than password2?\n {isPassword1Longer}");
+
+        //!=
+        bool arePasswordsDifferent = password1 != password2;
+        Console.WriteLine($"Are passwords different?\n {arePasswordsDifferent}");
+
+        //++
+        password1++;
+        Console.WriteLine($"Password after ++ operator?\n {password1}");
+
+        //true
+        if (password1) 
+        {
+            Console.WriteLine($"Is password strong?\n true");
+        }
+        else
+        {
+            Console.WriteLine($"Is password strong?\n false");
+        }
+
+
+        // Проверка методов расширения
+        string middleSymbol = "password".MiddleSymbol();
+        Console.WriteLine($"Middle symbol of 'password'?\n {middleSymbol}");
+
+        bool isValidLength = password1.IsValidLength();
+        Console.WriteLine($"Is password of valid length?\n {isValidLength}");
+    }
+}

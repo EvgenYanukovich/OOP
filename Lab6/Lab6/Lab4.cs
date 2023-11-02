0497d5565166281b18529bdb938288c282ddebd2
Evgeniy Yanukovich <86297837+EvgenYanukovich@users.noreply.github.com>
2023-11-02 12:30:30 +0300

    Add files via upload

---
+++ Lab6/Lab6/Lab4.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+public interface IInformation
+{
+    bool Inform();
+}
+
+public abstract class Information
+{
+    public abstract bool Inform();
+}
+
+// Определение класса Товар
+public abstract partial class Product : Information, IInformation
+{
+    public string? Manufacturer { get; set; }
+    public decimal Price { get; set; }
+    public decimal Weight { get; set; }
+
+    // из абстрактного класса
+    public override bool Inform()
+    {
+        Console.WriteLine("Вызван метод Inform() из абстрактного класса Information.");
+        return true;
+    }
+
+    // из интерфейса
+    bool IInformation.Inform()
+    {
+        Console.WriteLine("Вызван метод Inform() из интерфейса IInformation.");
+        return false;
+    }
+}
+// Определение класса Мебель
+public class Furniture : Product
+{
+    public override string ToString()
+    {
+        return "Это мебель.";
+    }
+}
+
+// Определение класса Диван
+public sealed class Sofa : Furniture
+{
+    public override string ToString()
+    {
+        return "Это диван.";
+    }
+}
+
+// Определение класса Кровать
+public class Bed : Furniture
+{
+    public override string ToString()
+    {
+        return "Это кровать.";
+    }
+}
+
+// Определение класса Шкаф
+public class Wardrobe : Furniture
+{
+    public override string ToString()
+    {
+        return "Это шкаф.";
+    }
+}
+
+// Определение класса Шкаф-купе
+public class SlidingWardrobe : Wardrobe
+{
+    public override string ToString()
+    {
+        return "Это шкаф-купе.";
+    }
+}
+
+// Определение класса Вешалка
+public class Hanger : Furniture
+{
+    public override string ToString()
+    {
+        return "Это вешалка.";
+    }
+}
+
+// Определение класса Тумба
+public class BedsideTable : Furniture
+{
+    public override string ToString()
+    {
+        return "Это тумба.";
+    }
+}
+
+// Определение класса Стул
+public class Chair : Furniture
+{
+    public override string ToString()
+    {
+        return "Это стул.";
+    }
+}
+
+// Определение класса Printer 
+public class Printer
+{
+    public void IAmPrinting(Product someobj)
+    {
+        Console.WriteLine(someobj.ToString());
+    }
+}
+
+class Lab4
+{
+    void Test()
+    {
+        Sofa sofa = new Sofa();
+        Bed bed = new Bed();
+        Wardrobe wardrobe = new Wardrobe();
+
+        Product[] products = new Product[] { sofa, bed, wardrobe };
+
+        Printer printer = new Printer();
+
+        printer.IAmPrinting(sofa);
+        printer.IAmPrinting(bed);
+        printer.IAmPrinting(wardrobe);
+
+        Product product = new Sofa();
+
+        if (sofa is IInformation)
+        {
+            Console.WriteLine("Объект sofa поддерживает интерфейс IInformation.");
+            IInformation cloneableSofa = sofa;
+            cloneableSofa.Inform();
+        }
+
+        if (sofa is Information)
+        {
+            Console.WriteLine("Объект sofa является экземпляром абстрактного класса Information.");
+            Information baseSofa = sofa;
+            baseSofa.Inform();
+        }
+    } 
+    static void Main(string[] args)
+    {
+        new Lab4().Test();
+        new Lab5().Test();
+        Lab6.Test(3, 1, new int[3] { 1, 2, 3 }, "");
+
+    }
+}
---
+++ Lab6/Lab6/Lab6.cs
@@ -0,0 +1,128 @@
+﻿// Базовый пользовательский класс исключений
+using System.Collections;
+using System.Diagnostics;
+
+public class MyBaseException : Exception
+{
+    public string? Value { get; set; }
+    public MyBaseException(string message)
+        : base(message)
+    {
+    }
+}
+
+// Первый производный класс исключений
+public class MyDerivedException1 : MyBaseException
+{
+    public MyDerivedException1(string message)
+        : base(message)
+    {
+    }
+}
+
+// Второй производный класс исключений
+public class MyDerivedException2 : MyBaseException
+{
+    public MyDerivedException2(string message)
+        : base(message)
+    {
+    }
+}
+
+// Третий производный класс исключений
+public class MyDerivedException3 : MyBaseException
+{
+    public MyDerivedException3(string message)
+        : base(message)
+    {
+    }
+}
+
+public static class Logger
+{
+    public static void ConsoleLogger(MyBaseException exc)
+    {
+        Console.WriteLine("\n------Error");
+        Console.WriteLine($"Message: {exc.Message}");
+        Console.WriteLine($"Wrong name: {exc.Value}");
+        Console.WriteLine("Path: {0}", exc.TargetSite);
+        foreach (DictionaryEntry d in exc.Data)
+            Console.WriteLine("-> {0}, {1}", d.Key, d.Value);
+    }
+
+    public static void ConsoleLogger(SystemException exc)
+    {
+        Console.WriteLine("\n------Error");
+        Console.WriteLine($"Message: {exc.Message}");
+        Console.WriteLine("Path: {0}", exc.TargetSite);
+        foreach (DictionaryEntry d in exc.Data)
+            Console.WriteLine("-> {0}, {1}", d.Key, d.Value);
+    }
+}
+
+public class Lab6
+{
+    public static void Test(int x, int y, int[] arr, string path)
+    {
+        try
+        {
+            // Проверяем, не передан ли нулевой указатель в качестве массива
+            if (arr == null)
+            {
+                throw new ArgumentNullException("arr");
+            }
+
+            // Проверяем, не передан ли нулевой указатель в качестве пути к файлу
+            if (path == null)
+            {
+                throw new ArgumentNullException("path");
+            }
+
+            // Проверяем, не является ли путь к файлу пустой строкой
+            if (path == "")
+            {
+                throw new ArgumentException("path cannot be empty");
+            }
+
+            // Проверяем, не является ли делитель нулем
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            // Выполняем деление
+            int result = x / y;
+
+            // Проверяем, не выходит ли индекс за границы массива
+            if (x < 0 || x >= arr.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            int element = arr[x];
+
+            using (var reader = new StreamReader(path))
+            {
+                string? line = reader.ReadLine();
+                Console.WriteLine(line);
+
+
+                Debug.Assert(line == "", "Строка line пуста");
+            }
+        }
+        catch (MyBaseException e) 
+        {
+            Console.WriteLine($"MyBaseException: {e.Message}");
+            Logger.ConsoleLogger(e);
+        }
+        catch (SystemException e) // Обрабатываем стандартные исключения
+        {
+            Console.WriteLine($"SystemException: {e.Message}");
+            Logger.ConsoleLogger(e);
+        }
+        finally // Выполняем завершающие действия в любом случае
+        {
+            Console.WriteLine("DoSomething finished");
+        }
+    }
+}

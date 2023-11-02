9536ee695e9adcbbc259ca90caecb82204c9116d
Evgeniy Yanukovich <86297837+EvgenYanukovich@users.noreply.github.com>
2023-11-02 13:37:02 +0300

    Add files via upload

---
+++ Lab7/Lab7/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+// обобщенный интерфейс с операциями добавить, удалить, просмотреть.
+public interface IOperations<T>
+{
+    void Add(T item);
+    void Remove(T item);
+    T View(Predicate<T> predicate);
+}
+
+// обобщенный тип (класс) CollectionType<T>, в котором лежит обобщённая коллекция.
+public class CollectionType<T> : IOperations<T>
+{
+    private List<T> collection = new List<T>();
+
+    public void Add(T item)
+    {
+        try
+        {
+            collection.Add(item);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("Операция добавления завершена.");
+        }
+    }
+
+    public void Remove(T item)
+    {
+        try
+        {
+            collection.Remove(item);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("Операция удаления завершена.");
+        }
+    }
+
+    public T View(Predicate<T> predicate)
+    {
+        try
+        {
+            return collection.Find(predicate);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+            return default(T);
+        }
+        finally
+        {
+            Console.WriteLine("Операция просмотра завершена.");
+        }
+    }
+
+    //методы сохранения объектов типа CollectionType<T> в файл и чтения из него.
+    public void SaveToFile(string fileName)
+    {
+        try
+        {
+            var jsonString = JsonSerializer.Serialize(collection);
+            File.WriteAllText(fileName, jsonString);
+            Console.WriteLine("Коллекция успешно сохранена в файл.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при сохранении в файл: {ex.Message}");
+        }
+    }
+
+    public void LoadFromFile(string fileName)
+    {
+        try
+        {
+            var jsonString = File.ReadAllText(fileName);
+            collection = JsonSerializer.Deserialize<List<T>>(jsonString);
+            Console.WriteLine("Коллекция успешно загружена из файла.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при загрузке из файла: {ex.Message}");
+        }
+    }
+}
+
+// пользовательский класс
+public class UserClass
+{
+    public string? Name { get; set; }
+}
+
+// использование обобщения для стандартных типов данных
+class Program
+{
+    static void Main(string[] args)
+    {
+        var intCollection = new CollectionType<int>();
+        intCollection.Add(1);
+
+        var userClassCollection = new CollectionType<UserClass>();
+        userClassCollection.Add(new UserClass { Name = "Test" });
+
+        userClassCollection.SaveToFile("test.json");
+        userClassCollection.LoadFromFile("test.json");
+    }
+}
---
+++ Lab8/Lab8/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+class Programmer
+{
+    public delegate void RenameHandler(string oldName, string newName);
+    public event RenameHandler Renamed;
+
+    public delegate void NewPropertyHandler(string propertyName, string propertyValue);
+    public event NewPropertyHandler NewPropertyAdded;
+
+    private string _name;
+    public string Name
+    {
+        get { return _name; }
+        set
+        {
+            if (_name != value)
+            {
+                Renamed?.Invoke(_name, value);
+                _name = value;
+            }
+        }
+    }
+
+    private string _property;
+    public string Property
+    {
+        get { return _property; }
+        set
+        {
+            if (_property != value)
+            {
+                NewPropertyAdded?.Invoke("Property", value);
+                _property = value;
+            }
+        }
+    }
+}
+
+class Language
+{
+    public string Name { get; set; }
+    public string Version { get; set; }
+
+    public Language(string name, string version)
+    {
+        Name = name;
+        Version = version;
+    }
+
+    public void OnRenamed(string oldName, string newName)
+    {
+        if (Name == oldName)
+        {
+            Name = newName;
+            Console.WriteLine($"Язык {oldName} был переименован в {newName}");
+        }
+    }
+
+    public void OnNewPropertyAdded(string propertyName, string propertyValue)
+    {
+        Console.WriteLine($"Добавлено новое свойство: {propertyName} со значением: {propertyValue}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Programmer programmer = new Programmer();
+        
+        Language csharp = new Language("C#", "9.0");
+        Language python = new Language("Python", "3.9");
+
+        programmer.Renamed += csharp.OnRenamed;
+        programmer.Renamed += python.OnRenamed;
+
+        programmer.NewPropertyAdded += csharp.OnNewPropertyAdded;
+        
+        programmer.Name = "John Doe";
+        programmer.Property = "Senior Developer";
+    }
+}

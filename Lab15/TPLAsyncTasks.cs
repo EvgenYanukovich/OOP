b780f6b13c77200c122f777388047d823640549d
Evgeniy Yanukovich <86297837+EvgenYanukovich@users.noreply.github.com>
2023-12-14 10:35:29 +0300

    Add files via upload

---
+++ Lab15/TPLAsyncTasks.cs
@@ -0,0 +1,21 @@
+﻿
+namespace OOP_lab_15 {
+    internal static class TPLAsyncTasks {
+        //синтаксис async/await
+        public static async Task InvokeAsyncTask() {
+            Console.WriteLine("async method start");
+
+            await DoSomethingAsync();
+
+            Console.WriteLine("wait untill done");
+        }
+
+        private static async Task DoSomethingAsync() {
+            Console.WriteLine("async operation start");
+
+            await Task.Delay(2000);
+
+            Console.WriteLine("done.");
+        }
+    }
+}
